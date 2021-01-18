569644f7137fe74cad3fd66dc7ee7a4a7bd1cc44
Bryan Escudero <Ajefferson419@yahoo.com>
2021-01-18 02:11:44 -0800

    deploy to heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/guarded-brushlands",
+  process.env.MONGODB_URI || "mongodb://localhost/guarded-brushlands-39016",
   {
     useCreateIndex: true,
     useNewUrlParser: true
